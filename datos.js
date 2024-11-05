9ada2c0fa810e6538174a00835da019709f8e5b1
1VIDIX <131642338+1VIDIX@users.noreply.github.com>
2024-11-04 20:39:09 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -218,7 +218,7 @@ document.getElementById('yearSelect').addEventListener('change', function() {
 
 // Llama a la función llenarTabla con el año inicial al cargar la página
 document.addEventListener('DOMContentLoaded', function() {
-    llenarTabla('2012');
+    llenarTabla('2024');
 });
 
 
